9dcaba0d874f5c240ef3a484a05e01e827b3b626
SNL23 <steph@henahosa.com>
2023-03-26 14:54:38 -0700

    added conditional statment to the js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,7 @@
+(function ($) {
+  //Adds the date to the header
+  var today = dayjs().hour();
 
-$(document).ready(function () {
   //this code add and event listener of "click" 
   $(".saveBtn").on("click", function () {
     //this code retrieves the textarea value and the id attribute
@@ -8,28 +10,25 @@ $(document).ready(function () {
     //this code saves the textarea value to the local storage
     localStorage.setItem(id, textarea);
   });
-});
+
 
 $(".time-block").each(function () {
-  //conditional statments for past, present and future css class
-  //get hour from div
-  var currentHour =
-  //get current hour from dayjs
-  var 
-  //compare the values down below
-  if () {
-    
-  } else if () {
-
+  var currentHour = $(this).attr("id").split("-")[1];
+  console.log($(this).attr("id").split("-")[1],);
+  console.log(currentHour);
+  if (currentHour < today) {
+    $(this).addClass("past")
+  } else if (currentHour == today) {
+    $(this).addClass("present")
   } else {
-
+    $(this).addClass("future")
   }
-
   //this code retrieves the users saved info
   var id = $(this).attr("id");
   var val = localStorage.getItem(id);
   $(this).children("textarea").val(val);
 });
+})(jQuery);
 
 //This code goes to each class of "time-block" and takes the hour from the id
 var today = dayjs();
@@ -37,8 +36,5 @@ var currentHour = today.format("H");
 $(".time-block").each(function () {
   var timeBlock = $(this);
   var hour = timeBlock.attr("id").split("-").pop();
+})
 
-});
-//Adds the date to the header
-var today = dayjs();
-$('#currentDay').text(today.format('dddd, MMMM D'));
