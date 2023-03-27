f3551e5331e0dd923ab3632a4850cb7a2b314364
SNL23 <steph@henahosa.com>
2023-03-26 20:15:37 -0700

    added a description to the README file and added and assets folder also finished adding comments in the js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,35 @@
 (function ($) {
-  //Adds the date to the header
-  var today = dayjs().hour();
+  //adds the date to the header
+  var date = dayjs();
+  $("#currentDay").text(date.format('dddd, MMMM D'));
 
+  var today = dayjs().hour();
   //this code add and event listener of "click" 
   $(".saveBtn").on("click", function () {
-    //this code retrieves the textarea value and the id attribute
+    //this code retrieves the value of the textarea element and the id attribute of the parent element
     var textarea = $(this).siblings("textarea").val();
     var id = $(this).parent().attr("id");
     //this code saves the textarea value to the local storage
     localStorage.setItem(id, textarea);
+    
   });
-
-
-$(".time-block").each(function () {
-  var currentHour = $(this).attr("id").split("-")[1];
-  console.log($(this).attr("id").split("-")[1],);
-  console.log(currentHour);
-  if (currentHour < today) {
-    $(this).addClass("past")
-  } else if (currentHour == today) {
-    $(this).addClass("present")
-  } else {
-    $(this).addClass("future")
-  }
-  //this code retrieves the users saved info
-  var id = $(this).attr("id");
-  var val = localStorage.getItem(id);
-  $(this).children("textarea").val(val);
-});
+  //loops through each elemnet 
+  $(".time-block").each(function () {
+//this code retrieves the users saved info 
+    var id = $(this).attr("id");
+    var val = localStorage.getItem(id);
+    $(this).children("textarea").val(val);
+    // conditional statment to add past,present or future 
+    //to the element which in turn adds a certain style.
+    var currentHour = $(this).attr("id").split("-")[1];
+    if (currentHour < today) {
+      $(this).addClass("past")
+    } else if (currentHour == today) {
+      $(this).addClass("present")
+    } else {
+      $(this).addClass("future")
+    }
+  });
 })(jQuery);
 
-//This code goes to each class of "time-block" and takes the hour from the id
-var today = dayjs();
-var currentHour = today.format("H");
-$(".time-block").each(function () {
-  var timeBlock = $(this);
-  var hour = timeBlock.attr("id").split("-").pop();
-})
 
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # work-day-scheduler
+
+## Description
+
+This webpage was created for anyone with a busy schedule so that they can manage thier time more effectively. This project was build so that the user can create a schedule and add important events to the planner. In this project I was able to add the current day to the top of the planner. I also added a click function, so that when the saved button was clicked it saved users input in the local storage. Lastly, I was able to color code the time blocks to indicate whether it is in the past, present or future. While working on this project, I learned how Dayjs works to input dates and time onto a webpage and how to format it. I also learned how the code for local storage is implamented and how to retrieve the saved data. 
+
+## Installation
+
+N/A
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![alt text](assets/images/screenshot.png)
+
+
+## License
+
+Refer to the Repo
